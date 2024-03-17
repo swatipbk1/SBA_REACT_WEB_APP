0f7803620168ee25c6c464252426837fc38bd2a7
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-17 14:23:26 -0700

    c

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ const App = () => {
         <Route path="/" element={<News category="general" />} />
         <Route path="/business" element={<News category="business" />} />
         <Route path="/entertainment" element={<News category="entertainment" />} />
-        
+        <Route path="/health" element={<News category="health" />} />
+        <Route path="/science" element={<News category="science" />} />
+        <Route path="/sports" element={<News category="sports" />} />
+        <Route path="/technology" element={<News category="technology" />} />
+      </Routes>
+    </Router>
   );
 }
 
