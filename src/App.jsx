af878152436a9fbda224b2ad64c70e7f1d5f83a5
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-17 14:22:51 -0700

    c

--- src/App.jsx
+++ src/App.jsx
@@ -5,4 +5,14 @@ import News from './Components/News/News';
 
 const App = () => {
   return (
-    
+    <Router>
+      <Navigation />
+      <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+        <h1 style={{ fontWeight: 'bold', marginBottom: '10px' }}>Top Headlines</h1>
+        <h2 style={{ fontStyle: 'italic', color: '#555', marginTop: '0' }}>from ABP News</h2>
+      </div>
+   
+  );
+}
+
+export default App;
