ebbec92b15f29e0f974bd2950b8146ea8c96b260
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-17 14:40:59 -0700

    c

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -3,4 +3,10 @@ import './navigation.css'; // Import CSS file
 
 const Navigation = () => {
   return (
-  
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">ABPNEWS</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+       ;
