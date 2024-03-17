947cd6ab3d0ef4232103e26aa79ea0eed49693b6
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-16 23:56:36 -0700

    c

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './navigation.css'; // Import CSS file
+
+const Navigation = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid"></div>
