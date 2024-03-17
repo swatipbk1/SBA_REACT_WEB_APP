ce55baeedd8bf8bbbdf0dabd1668a1e4f16b6f3f
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-17 14:41:14 -0700

    c

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -9,4 +9,4 @@ const Navigation = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-       ;
+   
