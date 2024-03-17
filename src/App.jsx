2725fd09b8b59eb77b8df1e867e203a64cda77eb
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-17 14:22:28 -0700

    c

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './Components/Navigation/Navigation';
+import News from './Components/News/News';
+
+const App = () => {
+  return (
+    
