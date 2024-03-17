5060f4356701f1356ad2fbe1cc724dd72031f72e
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-16 23:57:46 -0700

    c

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,8 +4,8 @@ import './navigation.css'; // Import CSS file
 const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid"></div>
-      <a className="navbar-brand" href="/">INDIA-TODAY</a>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">INDIA-TODAY</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -13,7 +13,7 @@ const Navigation = () => {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
+            </li>
             <li className="nav-item"><a className="nav-link active" href="/business">Business</a></li>
             <li className="nav-item"><a className="nav-link active" href="/entertainment">Entertainment</a></li>
             <li className="nav-item"><a className="nav-link active" href="/health">Health</a></li>
@@ -21,3 +21,10 @@ const Navigation = () => {
             <li className="nav-item"><a className="nav-link active" href="/sports">Sports</a></li>
             <li className="nav-item"><a className="nav-link active" href="/technology">Technology</a></li>
           </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
