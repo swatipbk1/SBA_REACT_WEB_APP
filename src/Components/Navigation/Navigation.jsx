83221ee3fd39674c3bc46b145f8049d28add4994
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-18 23:05:21 -0700

    Update Navigation.jsx

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react';
+import './navigation.css'; // Import CSS file
+
+const Navigation = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">CNN</a>
+      
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       
