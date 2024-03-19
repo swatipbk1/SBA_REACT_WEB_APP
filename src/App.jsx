c28937c39a64e4bf78d1c57a79e8ddf5b6ca1965
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-18 23:11:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './Components/Navigation/Navigation';
+import News from './Components/News/News';
+import Footer from './Components/Footer/Footer'; // Import Footer component
+import './App.css'; // Import CSS file
+import LoadingBar from 'react-top-loading-bar';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+      
