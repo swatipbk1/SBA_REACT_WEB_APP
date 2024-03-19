3bbab7a5eb39850e63310660231fe4a818654cd7
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-18 23:13:14 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
 
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// Get the root container
+const rootContainer = document.getElementById('root');
+
