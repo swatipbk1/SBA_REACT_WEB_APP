e6dae7344d2bb6d1c4b1c5c8644b17996bb12532
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-18 22:33:43 -0700

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './Components/Navigation/Navigation';
 import News from './Components/News/News';
+import Footer from './Components/Footer/Footer'; // Import Footer component
+import './App.css'; // Import CSS file
+import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
   return (
     <Router>
-      <Navigation />
-      <div style={{ textAlign: 'center', paddingTop: '100px' }}>
-        <h1 style={{ fontWeight: 'bold', marginBottom: '10px' }}>Top Headlines</h1>
-        <h2 style={{ fontStyle: 'italic', color: '#555', marginTop: '0' }}>from ABP News</h2>
+      <div>
+        <Navigation />
+        <div className="top-heading">
+          <h1>Top Headlines</h1>
+          <h2>Real-Time News Coverage: Latest Updates</h2>
+        </div>
+        <Routes>
+          <Route path="/" element={<News category="general" />} />
+          <Route path="/business" element={<News category="business" />} />
+          <Route path="/entertainment" element={<News category="entertainment" />} />
+          <Route path="/health" element={<News category="health" />} />
+          <Route path="/science" element={<News category="science" />} />
+          <Route path="/sports" element={<News category="sports" />} />
+          <Route path="/technology" element={<News category="technology" />} />
+        </Routes>
+        <Footer />
       </div>
-      <Routes>
-        <Route path="/" element={<News category="general" />} />
-        <Route path="/business" element={<News category="business" />} />
-        <Route path="/entertainment" element={<News category="entertainment" />} />
-        <Route path="/health" element={<News category="health" />} />
-        <Route path="/science" element={<News category="science" />} />
-        <Route path="/sports" element={<News category="sports" />} />
-        <Route path="/technology" element={<News category="technology" />} />
-      </Routes>
     </Router>
   );
 }
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -5,7 +5,8 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">ABPNEWS</a>
+        <a className="navbar-brand" href="/">CNN</a>
+      
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
