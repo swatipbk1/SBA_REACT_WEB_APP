7cbcc8a31f3d864f60afd49f87a46075fa1078cd
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-18 23:08:01 -0700

    Update News.jsx

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,1 +1,60 @@
 
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+import "./News.css";
+import spinner from "./Spinner.gif";
+
+// // // Defined API key as a constant
+const API_KEY = "714ef9b8a6ef47d19b4bda6f4f0d100f";
+// const API_KEY = import.meta.env.VITE_API_KEY;
+
+
+const News = ({ category }) => { // Receive category prop
+  const [myNews, setMyNews] = useState([]);
+  const [loading, setLoading] = useState(true); // State to track loading status
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 6; // Number of articles per page
+
+  const fetchData = async () => {
+    try {
+      setLoading(true); // Set loading to true when fetching data
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}` // Use API_KEY constant
+      );
+      const data = await response.json();
+      // Filter out articles with null or undefined image or title or "Unknown" title
+      const filteredArticles = data.articles.filter(
+        (article) =>
+          article.urlToImage !== null &&
+          article.urlToImage !== undefined &&
+          article.title !== null &&
+          article.title !== undefined &&
+          article.title !== "Unknown"
+      );
+      setMyNews(filteredArticles);
+      setLoading(false); // Set loading to false after data is fetched
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false); // Set loading to false in case of an error
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [category]); // Fetch data whenever category changes
+
+  // Function to handle fetching data and setting loading state
+  const handleFetchData = () => {
+    fetchData();
+  };
+
+  // Logic to calculate the index of the first and last articles for the current page
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = myNews.slice(indexOfFirstArticle, indexOfLastArticle);
+
+  // Logic to paginate
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    handleFetchData(); // Fetch data for the new page
+  };
